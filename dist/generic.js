39fccf70faaca26f91c9b81675198164b8103600
Samiul-Alam-Shanto <samiulalamshantowpdev@gmail.com>
2026-05-12 09:52:47 +0600

    learned generics, interface and type assertion

---
+++ dist/generic.js
@@ -0,0 +1,27 @@
+"use strict";
+//dynamically generalize: Generic
+//mainly amra function e jemon dynamically kichu rcv korar jonno parameter and argument use kori .. temon typescipt e dynamically type define korar jonno genertics use kora hoy
+Object.defineProperty(exports, "__esModule", { value: true });
+// const friends: string[] = ["Mr. X", "Mr. Y", "Mr. Z"];
+const friends = ["Mr. X", "Mr. Y", "Mr. Z"];
+//const rollNumbers: number[] = [4, 7, 11];
+const rollNumbers = [4, 7, 11];
+//const isEligibleList: boolean[] = [true, false, true];
+const isEligibleList = [true, false, true];
+const userList = [
+    {
+        name: "Mr. X",
+        age: 22,
+    },
+    {
+        name: "Mr. Y",
+        age: 25,
+    },
+];
+const sqrFunc = (value) => {
+    return value * value;
+};
+sqrFunc(4);
+const coordinates1 = [20, 30];
+const coordinates2 = ["20", "30"];
+//# sourceMappingURL=generic.js.map
